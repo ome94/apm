c11411faa935171967db5a452df6a1bf4c7fcb3f
Mayowa Emmanuel Olabanji <mayowae@outlook.com>
2021-10-08 22:56:01 +0100

    Guard ProductDetailComponent From Invalid Addresses with canActivate

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,15 @@
 import { WelcomeComponent } from "./home/welcome.component";
 import { ProductDetailComponent } from "./products/product-detail.component";
+import { ProductDetailGuard } from "./products/product-detail.guard";
 import { ProductListComponent } from "./products/product-list.component";
 
 export const routes = [
   { path: 'products', component: ProductListComponent },
-  { path: 'products/:id', component: ProductDetailComponent },
+  { 
+    path: 'products/:id',
+    component: ProductDetailComponent,
+    canActivate: [ProductDetailGuard]
+  },
   { path: 'welcome', component: WelcomeComponent},
   { path: '', redirectTo: 'welcome', pathMatch: 'full'}
 ]
